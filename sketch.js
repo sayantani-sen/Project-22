5536b63a383eaa419c013a718660c9eb5efe8875
sayantani-sen <74294422+sayantani-sen@users.noreply.github.com>
2021-01-16 18:34:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,27 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+
+function preload(){
 	helicopterIMG=loadImage("helicopter.png")
 	packageIMG=loadImage("package.png")
 }
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
-	
 
-	packageSprite=createSprite(width/2, 80, 10,10);
+	engine = Engine.create();
+	world = engine.world;
+
+	var package_options = {
+		restitution: 0
+	}
+	
+	packageSprite=createSprite(width/2, 200, 10,10,package_options);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
@@ -27,30 +32,25 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-
-	engine = Engine.create();
-	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
 	World.add(world, packageBody);
 	
-
-	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
 
 	Engine.run(engine);
   
 }
 
-
 function draw() {
-  rectMode(CENTER);
   background(0);
+  rectMode(CENTER);
+  Engine.update(engine);
+  
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
 
+	keyPressed();
 
   drawSprites();
  
@@ -58,11 +58,8 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-		Matter.Body.setStatic(false);
-    
+    	Matter.Body.setStatic(packageBody,false)
+	
   }
 }
-
-
 
